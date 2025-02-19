7077a8889e5db3e3d8b86605a3f335d82d4480b9
Rianne Boumans <rianne.boumans@zuyd.nl>
2025-02-19 22:22:26 +0100

    Eerste poging om data op te halen direct vanuit BGG (Succesvol :) )

---
+++ js/getplays.js
@@ -0,0 +1,18 @@
+console.log("Working")
+
+/*
+d3.xml("https://boardgamegeek.com//xmlapi2/plays?username=Excali8ur", function(error, data) {
+    
+    d3.select("body")
+        .selectAll("p")
+        .data(data)
+        .enter()
+        .append("p")
+        .text(function(d) {
+            return d.name + ", " + d.location;
+        });
+
+});
+*/
+
+console.log("Done?")
